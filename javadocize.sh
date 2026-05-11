f84b4920067af270cbd98122d5e6ed525ab3a94e
Max S <maxschwarz@students.berkeley.net>
2026-05-11 18:55:25 +0000

    doing javadocs

--- javadocize.sh
+++ javadocize.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-cd /home/mxz-schwarz/JSON-parser
-
 git clone -b main --single-branch https://github.com/max-S-bot/JSON-parser
 
-javadoc -d /home/mxz-schwarz/JSON-parser -sourcepath /home/mxz-schwarz/JSON-parser/JSON-parser -subpackages io.github.mxz_schwarz.parser
+javadoc -d ./ -sourcepath ./JSON-parser -subpackages io.github.mxz_schwarz.parser
 
 rm -rf JSON-parser
